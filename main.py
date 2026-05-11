f7d4f23f9b8ff9c979e2437d0b4ced743895d551
zhuzheng217-sys <zhuzheng217@gmail.com>
2026-05-11 20:59:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import requests
 import sxtwl
 import os
 import pytz
+import time
+import hmac
+import hashlib
+import base64
+import urllib.parse
 
 # 中国时区
 tz = pytz.timezone('Asia/Shanghai')
@@ -18,6 +23,7 @@ day = sxtwl.fromSolar(
 )
 
 tg = "甲乙丙丁戊己庚辛壬癸"
+dz = "子丑寅卯辰巳午未申酉戌亥"
 
 # 月干
 month_gan = tg[day.getMonthGZ().tg]
@@ -25,21 +31,39 @@ month_gan = tg[day.getMonthGZ().tg]
 # 日干
 day_gan = tg[day.getDayGZ().tg]
 
-# 完整干支
-month_gz = tg[day.getMonthGZ().tg]
-month_gz += "子丑寅卯辰巳午未申酉戌亥"[day.getMonthGZ().dz]
+# 月柱
+month_gz = tg[day.getMonthGZ().tg] + dz[day.getMonthGZ().dz]
 
-day_gz = tg[day.getDayGZ().tg]
-day_gz += "子丑寅卯辰巳午未申酉戌亥"[day.getDayGZ().dz]
+# 日柱
+day_gz = tg[day.getDayGZ().tg] + dz[day.getDayGZ().dz]
 
 print("月干:", month_gan)
 print("日干:", day_gan)
 
-# 条件判断
+# 判断条件
 if month_gan in ["丙", "丁"] and day_gan in ["丙", "丁"]:
 
     webhook = os.environ["DINGTALK_WEBHOOK"]
+    secret = os.environ["DINGTALK_SECRET"]
 
+    # ===== 加签 =====
+    timestamp = str(round(time.time() * 1000))
+
+    string_to_sign = f'{timestamp}\n{secret}'
+
+    hmac_code = hmac.new(
+        secret.encode('utf-8'),
+        string_to_sign.encode('utf-8'),
+        digestmod=hashlib.sha256
+    ).digest()
+
+    sign = urllib.parse.quote_plus(
+        base64.b64encode(hmac_code)
+    )
+
+    url = f"{webhook}&timestamp={timestamp}&sign={sign}"
+
+    # ===== 消息 =====
     text = f"""
 黄历提醒
 
@@ -61,7 +85,7 @@ if month_gan in ["丙", "丁"] and day_gan in ["丙", "丁"]:
         }
     }
 
-    r = requests.post(webhook, json=data)
+    r = requests.post(url, json=data)
 
     print(r.text)
 
